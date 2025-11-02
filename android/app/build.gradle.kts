07917a9e73ac4224ba1d8bfa53d4f866af18a486
YASAR300 <everythingdecoding@gmail.com>
2025-11-02 22:44:13 +0530

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.climapredict.app.clima_predict"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26  // Required by tflite_flutter plugin
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
